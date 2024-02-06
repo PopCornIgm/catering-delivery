308007754cb4c3f96437d7f5ba80e787dd394a0b
PopCornIgm <popcornigorek.1993@gmail.com>
2024-02-06 21:49:57 +0700

    добавление карточек меню через классы

--- js/script.js
+++ js/script.js
@@ -39,7 +39,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // Timer
 
-  const deadline = ' 2024-02-02';
+  const deadline = ' 2025-02-02';
 
   function getTimeRemaining(endtime) {
     const t = Date.parse(endtime) - Date.parse(new Date()),
@@ -128,8 +128,8 @@ window.addEventListener('DOMContentLoaded', () => {
       closeModal();
     }
   });
-
-  const modalTimerId = setTimeout(openModal, 3000);
+  // закоментируем тайм аут на время работы
+  // const modalTimerId = setTimeout(openModal, 3000);
 
   function showModalByScroll() {
     if (
@@ -142,4 +142,132 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   window.addEventListener('scroll', showModalByScroll);
+  // использование классов для карточек
+
+  //   class MenuCard {
+  //     constructor(src, alt, title, descr, price, parentSelector) {
+  //       this.src = src;
+  //       this.alt = alt;
+  //       this.title = title;
+  //       this.descr = descr;
+  //       this.price = price;
+  //       this.parent = document.querySelector(parentSelector);
+  //       this.transfer = 75;
+  //       this.changeToRub();
+  //     }
+
+  //     changeToRub() {
+  //       this.price = this.price * this.transfer;
+  //     }
+
+  //     render() {
+  //       const element = document.createElement('div');
+  //       element.innerHTML = `
+  //             <div class="menu__item">
+  //                 <img src=${this.src} alt=${this.alt}>
+  //                 <h3 class="menu__item-subtitle">${this.title}</h3>
+  //                 <div class="menu__item-descr">${this.descr}</div>
+  //                 <div class="menu__item-divider"></div>
+  //                 <div class="menu__item-price">
+  //                     <div class="menu__item-cost">Цена:</div>
+  //                     <div class="menu__item-total"><span>${this.price}</span> Руб/день</div>
+  //                 </div>
+  //             </div>
+  //         `;
+  //       this.parent.append(element);
+  //     }
+  //   }
+
+  //   new MenuCard(
+  //     'img/tabs/vegy.jpg',
+  //     'vegy',
+  //     'Меню "Фитнес"',
+  //     'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+  //     9,
+  //     '.menu .container'
+  //   ).render();
+  //
+
+  // Стандартная запись
+  // const div = new MenyCard(
+  //   'img/tabs/vegy.jpg',
+  //   'vegy',
+  //   'Меню "Фитнес"',
+  //   'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+  //   50,
+  //   '.menu.container'
+  // );
+  // div.render();
+
+  // Создаем обект вот таким способом и сразу же вызываем на нем метод рендер?Мы его создали, на нем использовался метод и все, после этого он исчезает так как на него нет ссылок больше
+
+  // new MenyCard(
+  //   'img/tabs/vegy.jpg',
+  //   'vegy',
+  //   'Меню "Фитнес"',
+  //   'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+  //   50,
+  //   '.menu.container'
+  // ).render();
+
+  class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.transfer = 75;
+      this.parent = document.querySelector(parentSelector);
+      this.changeToRub();
+    }
+    changeToRub() {
+      this.price = this.price * this.transfer;
+    }
+
+    render() {
+      const element = document.createElement('div');
+      element.innerHTML = `<div class="container">
+      <div class="menu__item">
+        <img src=${this.src} alt=${this.alt} />
+        <h3 class="menu__item-subtitle">${this.title}</h3>
+        <div class="menu__item-descr">${this.descr}</div>
+        <div class="menu__item-divider"></div>
+        <div class="menu__item-price">
+          <div class="menu__item-cost">Цена:</div>
+          <div class="menu__item-total"><span>${this.price}</span>Руб/день</div>
+        </div>
+      </div>`;
+      this.parent.append(element);
+    }
+  }
+
+  const div = new MenuCard(
+    'img/tabs/vegy.jpg',
+    'vegy',
+    "Меню 'Фитнес'",
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов продуктов активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    10,
+    ' .menu .container'
+  );
+
+  div.render();
+
+  new MenuCard(
+    'img/tabs/post.jpg',
+    'post',
+    'Меню "Постное"',
+    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+    20,
+    '.menu .container'
+  ).render();
+
+  new MenuCard(
+    'img/tabs/elite.jpg',
+    'elite',
+    'Меню “Премиум”',
+    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    30,
+    '.menu .container'
+  ).render();
 });
